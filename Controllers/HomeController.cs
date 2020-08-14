5a25001f1d882949d7503c399bb6d525fc497b4c
Rpbts <51463854+Rpbts@users.noreply.github.com>
2020-08-14 07:06:12 -0700

    before building out RSVP

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using PartyInvites.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+
 
 namespace PartyInvites.Controllers
 {
@@ -15,29 +10,11 @@ namespace PartyInvites.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
+        public ViewResult RsvpForm()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
