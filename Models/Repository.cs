85771c692d27bc2eeeb6c409077d11a773431831
Rpbts <51463854+Rpbts@users.noreply.github.com>
2020-08-18 09:53:06 -0700

    g

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -8,8 +8,40 @@ namespace PartyInvites.Models
         public static IEnumerable<GuestResponse> Responses => responses;
         public static void AddResponse(GuestResponse response)
         {
+            if (responses.Count < 1)
+            {
+                AddData();
+            }
             responses.Add(response);
         }
 
+        static void AddData()
+        {
+            GuestResponse gr = new GuestResponse();
+            gr.Name = "Huey";
+            gr.WillAttend = true;
+            gr.Phone = "2145672234";
+            gr.Email = "rtyu@bgfcfh.nic";
+            responses.Add(gr);
+
+
+            gr = new GuestResponse();
+            gr.Name = "Mickey";
+            gr.WillAttend = true;
+            gr.Phone = "214567288";
+            gr.Email = "rtrfyu@bgfcfh.nic";
+            responses.Add(gr);
+             
+            gr = new GuestResponse();
+            gr.Name = "Clutch";
+            gr.WillAttend = true;
+            gr.Phone = "233567288";
+            gr.Email = "rtrfyu@bgfcfh.ggg";
+            responses.Add(gr);
+
+
+        }
+
     }
+
 }
