4bc1cd434a56dcfa0332e642edb5643277d61618
Rpbts <51463854+Rpbts@users.noreply.github.com>
2020-08-20 07:39:25 -0700

    with valudation.

--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -1,10 +1,18 @@
-﻿namespace PartyInvites.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required (ErrorMessage = "Name is required.")]
         public string Name { get; set; }
+        [Required (ErrorMessage = "Please enter Email address.")]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your Phone Number.")]
         public string Phone { get; set; }
+        [Required(ErrorMessage ="Please indicate whether you will attend.")]
         public bool? WillAttend { get; set; }
     }
 }
